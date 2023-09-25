9b3ce433e91044eff1945e976947152fbe1790aa
Yağız Koç <71765094+YagizKoc@users.noreply.github.com>
2023-09-25 13:16:36 +0300

    Add files via upload

---
+++ İstanbulKart Clone/js/app.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+
+    // Header searchbox controller
+
+    let openBTN         =       $('header .header-top .searchbox .open-search');
+    let closeBTN        =       $('header .header-top .searchbox .search-input div');
+    let searchBox       =       $('header .header-top .searchbox');
+
+
+    $(openBTN).click(function(){
+        $(searchBox).addClass('active');
+    })
+    $(closeBTN).click(function(){
+        $(searchBox).removeClass('active');
+    })
+
+    let mobileMenuOpen  =       $('header .open-mobile');
+    let mobileMenuClose =       $('.mobile-menu .close-btn');
+    let mobileMenu      =       $('.mobile-menu');
+    $(mobileMenuOpen).click(function(){
+        $(mobileMenu).addClass('active');
+    })
+    $(mobileMenuClose).click(function(){
+        $(mobileMenu).removeClass('active');
+    })
+
+})
+
+// Hero slider controller
+
+$('.hero-section .slider-area').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+
+function postsCarousel() {
+    var checkWidth = $(window).width();
+    var owlPost = $('.announcement .slider-section')
+    if (checkWidth < 992) {
+      if (typeof owlPost.data('owl.carousel') != 'undefined') {
+        owlPost.data('owl.carousel').destroy();
+      }
+      owlPost.removeClass('owl-carousel');
+    } else if (checkWidth > 991) {
+        $(owlPost).addClass('owl-carousel');
+        $(owlPost).owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:1
+                }
+            }
+        })
+        $( ".announcement .slider-section .owl-prev").html('<i class="fa fa-chevron-left"></i>');
+        $( ".announcement .slider-section .owl-next").html('<i class="fa fa-chevron-right"></i>');
+    }
+  }
+  
+  postsCarousel();
+  $(window).resize(postsCarousel);
+
